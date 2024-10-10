608548c9381d79af041b0e092bc526395dbade0a
Taylor Henderson <tph5595@verizon.net>
2024-10-10 11:13:09 -0400

    feat: poc ocaml

--- flake.nix
+++ flake.nix
@@ -1,46 +1,21 @@
 {
-  description = "Example Python development environment for Zero to Nix";
+  description = "A Nix-flake-based OCaml development environment";
 
-  # Flake inputs
-  inputs = {
-    nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/*.tar.gz";
-  };
+  inputs.nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/*.tar.gz";
 
-  # Flake outputs
   outputs = { self, nixpkgs }:
     let
-      # Systems supported
-      allSystems = [
-        "x86_64-linux" # 64-bit Intel/AMD Linux
-        "aarch64-linux" # 64-bit ARM Linux
-        "x86_64-darwin" # 64-bit Intel macOS
-        "aarch64-darwin" # 64-bit ARM macOS
-      ];
-
-      # Helper to provide system-specific attributes
-      forAllSystems = f: nixpkgs.lib.genAttrs allSystems (system: f {
+      supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      forEachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
         pkgs = import nixpkgs { inherit system; };
       });
     in
     {
-      # Development environment output
-      devShells = forAllSystems ({ pkgs }: {
-        default =
-          let
-            # Use Python 3.12
-            python = pkgs.python312;
-          in
-          pkgs.mkShell {
-            LD_LIBRARY_PATH = "${pkgs.stdenv.cc.cc.lib.outPath}/lib:${pkgs.lib.makeLibraryPath [pkgs.zlib]}:$LD_LIBRARY_PATH";
-            # The Nix packages provided in the environment
-            packages = with pkgs; [
-                poetry
-                # Python plus helper tools
-                (python.withPackages (ps: with ps; [
-                    pip
-              ]))
-            ];
-          };
+      devShells = forEachSupportedSystem ({ pkgs }: {
+        default = pkgs.mkShell {
+          packages = with pkgs; [ ocaml ] ++
+            (with pkgs.ocamlPackages; [ dune_3 odoc utop fmt core base batteries findlib]);
+        };
       });
     };
 }
